5d25079f150d839cb4d95496410e133f5aa7c27a
markhughes <markhughes@clearplanconsulting.com>
2025-12-30 20:22:57 -0600

    Created class for Dto projection

---
+++ Models/ImageDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace ClearStore.Models
+{
+    public class ImageDetail
+    {
+        public int? ImageId { get; set; }
+
+        public byte[]? ImageData { get; set; }
+
+        public string ImageName { get; set; } = string.Empty;
+    }
+}
