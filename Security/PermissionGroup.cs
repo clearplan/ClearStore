7a959112798a499f8f58964fd181b8e9157a19fb
markhughes <markhughes@clearplanconsulting.com>
2026-06-30 15:38:53 -0500

    Added group to manage addresses via Entra

--- Security/PermissionGroup.cs
+++ Security/PermissionGroup.cs
@@ -5,5 +5,6 @@
         public static string AppStoreAdmins = "38e4409d-629e-494a-baaa-34411214460d";
         public static string AppDevelopers = "8219e59d-ca2c-4da2-97a1-98755a289cd9";
         public static string AppAdministrators = "fe2de275-230d-4133-9c79-af2c5f34acf4";
+        public static string AppWebStoreOrderRecipients = "009818de-b8ca-4a9e-a5d6-411a09ce8fec";
     }
 }
